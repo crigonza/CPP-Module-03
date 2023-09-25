ee9cc75a75a8d2484803095269b9bc14a39821a1
crigonza <crigonza@Mac-mini-de-Cristobal.local>
2023-09-25 20:24:45 +0200

    ..

--- ex00/ClapTrap.cpp
+++ ex00/ClapTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: crigonza <crigonza@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 12:13:06 by crigonza          #+#    #+#             */
-/*   Updated: 2023/09/25 20:07:40 by crigonza         ###   ########.fr       */
+/*   Updated: 2023/09/25 20:24:37 by crigonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,20 @@ ClapTrap::ClapTrap(std::string name) : _name(name), _hp(10), _ep(10), _ad(0)
     std::cout << "ClapTrap" << name << "constructor." << std::endl;
 }
 
-ClapTrap::ClapTrap(ClapTrap const &clap) : _name()
+ClapTrap::ClapTrap(ClapTrap const &clap)
 {
-    
+    std::cout << "Claptrap" << this->_name << "copy constructor." << std::endl;
+    *this = clap;
 }
 
 ClapTrap    &Claptrap::operator=(ClapTrap const &clap)
 {
-
+    std::cout << "Claptrap assignment operator." << std::endl;
+    this->_name = clap._name;
+    this->_hp = clap._hp;
+    this->_ep = clap._hp;
+    this->_ad = clap._ad;
+    return (*this);
 }
 
 ClapTrap::~ClapTrap(void)
